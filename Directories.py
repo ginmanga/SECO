b6a219f307b835890f25bd215a3fddd813340647
ginmanga <paboenhj@outlook.com>
2019-04-07 09:40:00 +0800

    "first line"

---
+++ Directories.py
@@ -0,0 +1,10 @@
+import os
+
+directory = r'C:\Users\Panqiao\Documents\Research\SEC Online - 05042017\All'
+path = os.path.abspath(directory)
+print(os.listdir(path))
+#for i in directory:
+    #print(i)
+#fipath(0, directory, 0)
+
+#first, find path for all files
